fb60ac4a720f179e8ff625c9f4d033d0f50567b5
Georges Dupéron <georges.duperon@gmail.com>
2016-01-18 17:18:51 +0100

    Fixed bug with index.html pages.

--- graph-lib/make/make-indexes.sh
+++ graph-lib/make/make-indexes.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find "$@" -type d -print0 | xargs -n 1 -r0 bash -c '(shopt -s nullglob; cd "$0"; if (! test -e index.html) || test -e .make-index-html; then touch .make-index-html; echo "<html>" > index.html; for i in */ *.html; do if test "$i" != "index.htm"; then echo "<a href=\"$i\">$i</a>" >> index.html; fi; done; echo "</html>" >> index.html; fi)'
+find "$@" -type d -print0 | xargs -n 1 -r0 bash -c '(shopt -s nullglob; cd "$0"; if (! test -e index.html) || test -e .make-index-html; then touch .make-index-html; echo "<html><ul>" > index.html; for i in */ *.html; do if test "$i" != "index.html"; then echo "<li><a href=\"$i\">$i</a></li>" >> index.html; fi; done; echo "</ul></html>" >> index.html; fi)'
