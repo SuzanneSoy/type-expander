e7b9e41c984e7c02d7a159a8e6443d03e255c76d
Georges Dupéron <georges.duperon@gmail.com>
2016-02-26 23:30:11 +0100

    Revert "Notes about the various failures."
    This reverts commit 1743dd0e0463f0edc05ab6b8245be4cdf025e5e2.

--- graph-lib/graph/__DEBUG_graph6G-req.rkt
+++ graph-lib/graph/__DEBUG_graph6G-req.rkt
@@ -1,7 +1,7 @@
 #lang typed/racket
 
 (require "graph-6-rich-returns.lp2.rkt"
-         (except-in "../lib/low.rkt" ~>)
+         "../lib/low.rkt"
          "graph.lp2.rkt"
          "get.lp2.rkt"
          "../type-expander/type-expander.lp2.rkt"
