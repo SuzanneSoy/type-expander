fb484502ba1e9a051f501946936ee65dd537ac61
Georges Dupéron <georges.duperon@gmail.com>
2016-04-05 13:08:54 +0200

    Migrate lib/ to jsmaniac/phc-toolkit project: removed lib/… files.

--- graph-lib/lib/low/set.rkt
+++
@@ -1,6 +0,0 @@
-#lang typed/racket
-(require "typed-untyped.rkt")
-(define-typed/untyped-modules #:no-test
-  (provide set-map→set)
-  (: set-map→set (∀ (e b) (→ (Setof e) (→ e b) (Setof b))))
-  (define (set-map→set s f) (list->set (set-map s f))))
--- graph-lib/lib/untyped.rkt
+++
@@ -1,4 +0,0 @@
-#lang typed/racket
-
-(require "low.rkt")
-(require/provide "untyped/for-star-list-star.rkt")
